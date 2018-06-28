2f5b0c160fe51c9d618ee68c5c4663408671817a
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-06-28 15:49:08 -0300

    finalizado

--- Condicao11.sql
+++ Condicao11.sql
@@ -1,4 +1,4 @@
 ﻿--Condição11
 
-UPDATE alunos SET data_nascimento = 31 WHERE DAY(data_nascimento) = 30;
+UPDATE alunos SET data_nascimento = DATEFROMPARTS(YEAR(data_nscimento),MONTH(data_nascimento), 30) WHERE DAY(data_nascimento) = 31;
 SELECT * FROm alunos;
---
+++ Condicao13.sql
@@ -0,0 +1,5 @@
+﻿--Condição13
+
+UPDATE alunos SET data_nascimento = DATEFROMPARTS(2018,06, Day(data_nascimento)) 
+WHERE MONTH(data_nascimento) = 07;
+SELECT* FROM alunos;
